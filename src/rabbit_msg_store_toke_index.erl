71410dd73b0701cdfd9db7b131cd710d741995fc
Daniil Fedotov <dfedotov@pivotal.io>
2017-02-23 17:00:49 +0000

    Rename callback function

--- src/rabbit_msg_store_toke_index.erl
+++ src/rabbit_msg_store_toke_index.erl
@@ -28,7 +28,7 @@
 
 -export([new/1, recover/1,
          lookup/2, insert/2, update/2, update_fields/3, delete/2,
-         delete_object/2, delete_by_file/2, terminate/1]).
+         delete_object/2, cleanup_undefined_file/1, terminate/1]).
 
 -define(FILENAME, "msg_store_toke.tch").
 
@@ -84,12 +84,12 @@ delete(Key, Toke) ->
 delete_object(Obj = #msg_location { msg_id = MsgId }, Toke) ->
     ok = toke_drv:delete_if_value_eq(Toke, MsgId, term_to_binary(Obj)).
 
-delete_by_file(File, Toke) ->
+cleanup_undefined_file(Toke) ->
     DeleteMe = toke_drv:fold(
                  fun (Key, Obj, Acc) ->
                          case (binary_to_term(Obj))#msg_location.file of
-                             File -> [Key | Acc];
-                             _    -> Acc
+                             undefined -> [Key | Acc];
+                             _         -> Acc
                          end
                  end, [], Toke),
     [ok = toke_drv:delete(Toke, Key) || Key <- DeleteMe],
